ef21319da8c723b3762dcb58976075c32ae081aa
Sankalp <sankalpbarapatre@gmail.com>
2026-09-22 11:59:41 +0530

    Create musicLibrary.py with song entries

---
+++ musicLibrary.py
@@ -0,0 +1,7 @@
+music = {
+    "co2" : "https://youtu.be/U2SVCCENLjE?si=xJOQ1mnDNEWZETR8",
+    "playdate" : "https://youtu.be/kknKs7cAcO8?si=FlX-Kqacu4W5eSMv",
+    "kalyani" : "https://youtu.be/xvT1jH8B9AM?si=3KRwMXdMR6PQpW23",
+  # Add your favourite songs and their URL here
+  
+}
